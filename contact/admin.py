3001c3b55704944fca4c09a55ab53793282c20b2
Diego Santos <diegosfaria022@hotmail.com>
2025-02-07 09:51:15 -0300

    Model etc

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from contact.models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id','first_name', 'last_name', 'phone','email',
+    ordering = 'id',
+    list_filter = 'created_date',
+    search_fields = 'id', 'first_name', 'last_name', 'email',
+    list_per_page = 10
+    list_max_show_all = 50
+    list_editable = 'first_name', 'last_name', 'phone',
